d2370b1cecffc4163056c0cd1370a0c5a6b6f68d
Kirill <kirill.baikin@gmail.com>
2022-09-06 00:19:50 +0300

    Some updates

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,15 @@ const {Sequelize} = require('sequelize');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-	const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+	let DB_URL = `postgres://postgres:${process.env.DB_PASSWORD}@db:5432/postgres`
+	if (process.env.NODE_ENV === 'development') {
+		DB_URL = `postgres://postgres:${process.env.DB_PASSWORD}@localhost:5439/postgres`
+	}
+
+	const sequelize = new Sequelize(DB_URL);
 	const users = await sequelize.query('SELECT * FROM users', {type: sequelize.QueryTypes.SELECT})
-	res.render('index', {title: 'Express', users, data: process.env});
+	res.render('index', {title: 'Express', users, data: {...process.env, ...{DATABASE_URL: undefined, DB_PASSWORD: undefined}}});
 });
 
 module.exports = router;
