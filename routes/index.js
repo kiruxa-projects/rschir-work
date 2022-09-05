27e0a2fdf3dd5cb3365cac7635abd74cf685da9b
Kirill <kirill.baikin@gmail.com>
2022-09-05 22:28:09 +0300

    Some updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const {Sequelize} = require('sequelize');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+	const sequelize = new Sequelize(process.env.DATABASE_URL);
+	const users = await sequelize.query('SELECT * FROM users', {type: sequelize.QueryTypes.SELECT})
+	res.render('index', {title: 'Express', users, data: process.env});
 });
 
 module.exports = router;
